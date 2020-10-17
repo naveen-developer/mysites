334f15d59db05c2ee94ce074967bdc883add0265
naveen <naveenofficial100@gmail.com>
2020-10-17 12:23:27 +0530

    Updates

---
+++ precache-manifest.e4feec5c18f0c821be11fa9bf7e266dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ffe8af1f501ac662de0ccbe33c3fd94",
+    "url": "/mysites/index.html"
+  },
+  {
+    "revision": "925c29f4d1f571a71770",
+    "url": "/mysites/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "81122669a8420c5ba291",
+    "url": "/mysites/static/css/main.6134d598.chunk.css"
+  },
+  {
+    "revision": "925c29f4d1f571a71770",
+    "url": "/mysites/static/js/2.d9b74495.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mysites/static/js/2.d9b74495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81122669a8420c5ba291",
+    "url": "/mysites/static/js/main.d68ff047.chunk.js"
+  },
+  {
+    "revision": "b14a2544f22b3fedd05e",
+    "url": "/mysites/static/js/runtime-main.45ee4d41.js"
+  }
+]);
